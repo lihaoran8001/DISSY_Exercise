195a6deff7448cd1e21396f83c80b21dc7c1b05b
lihaoran8001 <42938579+lihaoran8001@users.noreply.github.com>
2022-09-20 14:01:46 +0200

    apply new codes

--- Handin2/P2P_ledger_6node_new.go
+++ Handin2/P2P_ledger_6node_new.go
@@ -7,7 +7,33 @@ import (
 	"net"
 	"strconv"
 	"time"
+	"sync"
 )
+
+type Transaction struct {
+	ID string
+	From string
+	To string
+	Amount int
+}
+
+type Ledger struct {
+	Accounts map[string]int
+	lock sync.Mutex
+}
+
+func MakeLedger() *Ledger {
+	ledger := new(Ledger)
+	ledger.Accounts = make(map[string]int)
+	return ledger
+}
+
+func (l *Ledger) Transact(t *Transaction) {
+	l.lock.Lock() ; defer l.lock.Unlock()
+	
+	l.Accounts[t.From] -= t.Amount
+	l.Accounts[t.To] += t.Amount
+}
 
 type Message struct {
 	MsgType    string
@@ -23,7 +49,15 @@ type Peer struct {
 	Addr  string
 	Port  int
 	Peers []PeerInfo
+	ledger Ledger
 }
+
+func (p *Peer) FloodTransaction(tx *Transaction){
+	tx_content, _ := json.Marshal(*tx)
+	TxMsg := Message{"Transaction", string(tx_content)}
+	p.FloodMessage(TxMsg)
+}
+
 
 func (p *Peer) HandleConnection(conn net.Conn) {
 	defer conn.Close()
@@ -55,8 +89,11 @@ func (p *Peer) HandleConnection(conn net.Conn) {
 				var receivedJoin []PeerInfo
 				receivedJoin = append(receivedJoin, *receivedJoinPeer)
 				p.recordPeers(receivedJoin)
+			case "Transaction":
+				receivedTx := new(Transaction)
+				json.Unmarshal([]byte(receivedMessage.MsgContent), receivedTx)
+				p.ledger.Transact(receivedTx)
 			}
-
 		}
 	}
 }
@@ -96,35 +133,32 @@ func (p *Peer) Connect(addr string, port int) {
 	go p.Listen()
 }
 
-func (p *Peer) floodJoin() {
+func (p *Peer) FloodMessage(msg Message){
 	for _, selfPeerInfo := range p.Peers {
 		if p.Addr == selfPeerInfo.Addr && p.Port == selfPeerInfo.Port {
 			continue
 		}
 		target := selfPeerInfo.Addr + ":" + strconv.Itoa(selfPeerInfo.Port)
-		p.log("target: " + target)
-		// netAddr := &net.TCPAddr{Port: p.Port}
-		// d := net.Dialer{LocalAddr: netAddr}
+		p.log("flooding target: " + target)
 		conn, err := net.Dial("tcp", target)
 		if err != nil {
 			// address or port invalid
-			// start its own network
-			p.log("floodJoin falied" + err.Error())
-
+			p.log("flooding falied" + err.Error())
 			continue
 		}
 		defer conn.Close()
-
-		joinPeerInfo := PeerInfo{p.Addr, p.Port}
-		join_data, _ := json.Marshal(joinPeerInfo)
-		joinInfoMsg := Message{"joinMessage", string(join_data)}
-
-		data, _ := json.Marshal(joinInfoMsg)
+		data, _ := json.Marshal(msg)
 		writer := bufio.NewWriter(conn)
 		writer.Write(append(data, '\n'))
 		writer.Flush()
-		p.log("send joinMessage")
+		p.log("flooding message" + msg.MsgType + msg.MsgContent)
 	}
+}
+func (p *Peer) floodJoin() {
+	joinPeerInfo := PeerInfo{p.Addr, p.Port}
+	join_data, _ := json.Marshal(joinPeerInfo)
+	joinInfoMsg := Message{"joinMessage", string(join_data)}
+	p.FloodMessage(joinInfoMsg)
 }
 
 func (p *Peer) log(content string) {
@@ -176,42 +210,24 @@ func (p *Peer) recordPeers(receivedPeers []PeerInfo) {
 	}
 }
 
-// func (p *Peer) FloodMessage()
-
 func main() {
-	p1 := Peer{"127.0.0.1", 50001, []PeerInfo{}}
+	p1 := Peer{Addr:"127.0.0.1", Port:50001}
 	p1.Connect("127.0.0.1", 99999)
-
-	// time.Sleep(2 * time.Second)
-
-	p2 := Peer{"127.0.0.1", 50002, []PeerInfo{}}
+	time.Sleep(1 * time.Second)
+	p2 := Peer{Addr:"127.0.0.1", Port:50002}
 	p2.Connect("127.0.0.1", 50001)
-
-	// time.Sleep(2 * time.Second)
-
-	p3 := Peer{"127.0.0.1", 50003, []PeerInfo{}}
+	time.Sleep(1 * time.Second)
+	p3 := Peer{Addr:"127.0.0.1", Port:50003}
 	p3.Connect("127.0.0.1", 50002)
-
-	p4 := Peer{"127.0.0.1", 50004, []PeerInfo{}}
+	time.Sleep(1 * time.Second)
+	p4 := Peer{Addr:"127.0.0.1", Port:50004}
 	p4.Connect("127.0.0.1", 50002)
-
-	p5 := Peer{"127.0.0.1", 50005, []PeerInfo{}}
+	time.Sleep(1 * time.Second)
+	p5 := Peer{Addr:"127.0.0.1", Port:50005}
 	p5.Connect("127.0.0.1", 50003)
-
-	p6 := Peer{"127.0.0.1", 50006, []PeerInfo{}}
-	p6.Connect("127.0.0.1", 50004)
-
-	p7 := Peer{"127.0.0.1", 50007, []PeerInfo{}}
-	p7.Connect("127.0.0.1", 50004)
-
-	p8 := Peer{"127.0.0.1", 50008, []PeerInfo{}}
-	p8.Connect("127.0.0.1", 50005)
-
-	p9 := Peer{"127.0.0.1", 50009, []PeerInfo{}}
-	p9.Connect("127.0.0.1", 50005)
-
-	p10 := Peer{"127.0.0.1", 50010, []PeerInfo{}}
-	p10.Connect("127.0.0.1", 50007)
+	time.Sleep(1 * time.Second)
+	p6 := Peer{Addr:"127.0.0.1", Port:50006}
+	p6.Connect("127.0.0.1", 50001)
 
 	time.Sleep(1 * time.Second)
 	p1.log("updated peers info")
@@ -220,10 +236,58 @@ func main() {
 	p4.log("updated peers info")
 	p5.log("updated peers info")
 	p6.log("updated peers info")
-	p7.log("updated peers info")
-	p8.log("updated peers info")
-	p9.log("updated peers info")
-	p10.log("updated peers info")
+	
 
+	
 	select {}
 }
+
+// func main() {
+// 	p1 := Peer{"127.0.0.1", 50001, []PeerInfo{}}
+// 	p1.Connect("127.0.0.1", 99999)
+
+// 	// time.Sleep(2 * time.Second)
+
+// 	p2 := Peer{"127.0.0.1", 50002, []PeerInfo{}}
+// 	p2.Connect("127.0.0.1", 50001)
+
+// 	// time.Sleep(2 * time.Second)
+
+// 	p3 := Peer{"127.0.0.1", 50003, []PeerInfo{}}
+// 	p3.Connect("127.0.0.1", 50002)
+
+// 	p4 := Peer{"127.0.0.1", 50004, []PeerInfo{}}
+// 	p4.Connect("127.0.0.1", 50002)
+
+// 	p5 := Peer{"127.0.0.1", 50005, []PeerInfo{}}
+// 	p5.Connect("127.0.0.1", 50003)
+
+// 	p6 := Peer{"127.0.0.1", 50006, []PeerInfo{}}
+// 	p6.Connect("127.0.0.1", 50004)
+
+// 	p7 := Peer{"127.0.0.1", 50007, []PeerInfo{}}
+// 	p7.Connect("127.0.0.1", 50004)
+
+// 	p8 := Peer{"127.0.0.1", 50008, []PeerInfo{}}
+// 	p8.Connect("127.0.0.1", 50005)
+
+// 	p9 := Peer{"127.0.0.1", 50009, []PeerInfo{}}
+// 	p9.Connect("127.0.0.1", 50005)
+
+// 	p10 := Peer{"127.0.0.1", 50010, []PeerInfo{}}
+// 	p10.Connect("127.0.0.1", 50007)
+
+// 	time.Sleep(1 * time.Second)
+// 	p1.log("updated peers info")
+// 	p2.log("updated peers info")
+// 	p3.log("updated peers info")
+// 	p4.log("updated peers info")
+// 	p5.log("updated peers info")
+// 	p6.log("updated peers info")
+// 	p7.log("updated peers info")
+// 	p8.log("updated peers info")
+// 	p9.log("updated peers info")
+// 	p10.log("updated peers info")
+
+// 	select {}
+// }
